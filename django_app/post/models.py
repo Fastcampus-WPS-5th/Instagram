35e4b7492a9884df49c0ac15f719fe0a477811d5
Lee <dev@azelf.com>
2017-06-12 14:18:14 +0900

    CommentLike모델 추가

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -14,8 +14,36 @@ class Post(models.Model):
     photo = models.ImageField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    like_users = models.ManyToManyField(User, related_name='like_posts')
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_posts',
+        through='PostLike',
+    )
     tags = models.ManyToManyField('Tag')
+
+    def add_comment(self, user, content):
+        # 자신을 post로 갖고, 전달받은 user를 author로 가지며
+        # content를 content필드내용으로 넣는 Comment객체 생성
+        return self.comment_set.create(author=user, content=content)
+
+    def add_tag(self, tag_name):
+        # tags에 tag매개변수로 전달된 값(str)을
+        # name으로 갖는 Tag객체를 (이미 존재하면)가져오고 없으면 생성하여
+        # 자신의 tags에 추가
+        tag, tag_created = Tag.objects.get_or_create(name=tag_name)
+        if not self.tags.filter(id=tag.id).exists():
+            self.tags.add(tag)
+
+    @property
+    def like_count(self):
+        # 자신을 like하고 있는 user수 리턴
+        return self.like_users.count()
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
@@ -24,6 +52,17 @@ class Comment(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        User,
+        through='CommentLike',
+        related_name='like_comments',
+    )
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Tag(models.Model):

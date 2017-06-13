2d402a86552b9bf06573943c84dff749621bea35
Lee <dev@azelf.com>
2017-06-13 11:17:16 +0900

    post_detail view, urls, template설정

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -2,5 +2,19 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # url() 사용법
+    # https://docs.djangoproject.com/en/1.11/ref/urls/#url
+
+    # post_list와 매칭
+    # /post/$
     url(r'^$', views.post_list),
+
+    # post_detail과 매칭
+    # /post/3/$, /post/35/$
+    # 정규표현식에서 매칭된 그룹을 위치인수로 반환하는 방법
+    # url(r'^(\d+)/$', views.post_detail),
+
+    # 정규표현식에서 매칭된 그룹을 키워드인수로 반환하는 방법
+    # 그룹의 가장 앞 부분에 ?P<패턴이름>을 지정
+    url(r'^(?P<post_pk>\d+)/$', views.post_detail),
 ]
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
 from .models import Post
 
 
@@ -15,8 +18,36 @@ def post_list(request):
 
 
 def post_detail(request, post_pk):
-    # post_pk에 해당하는 Post객체를 리턴, 보여줌
-    pass
+    # Model(DB)에서 post_pk에 해당하는 Post객체를 가져와 변수에 할당
+    # ModelManager의 get메서드를 사용해서 단 한개의 객체만 가져온다
+    # https://docs.djangoproject.com/en/1.11/ref/models/querysets/#get
+    post = Post.objects.get(pk=post_pk)
+    # post = Post.objects.get(id=post_pk)는 위와 같음
+
+    # request에 대해 response를 돌려줄때는 HttpResponse나 render를 사용가능
+    # template을 사용하려면 render함수를 사용한다
+    # render함수는
+    #   django.template.loader.get_template함수와
+    #   django.http.HttpResponse함수를 축약해 놓은 shortcut이다
+    #       https://docs.djangoproject.com/en/1.11/topics/http/shortcuts/#render
+
+    # ! 이 뷰에서는 render를 사용하지 않고, 전체 과정(loader, HttpResponse)을 기술
+    # Django가 템플릿을 검색할 수 있는 모든 디렉토리를 순회하며
+    # 인자로 주어진 문자열값과 일치하는 템플릿이 있는지 확인 후,
+    # 결과를 리턴 (django.template.backends.django.Template클래스형 객체)
+    # get_template()메서드
+    #   https://docs.djangoproject.com/en/1.11/topics/templates/#django.template.loader.get_template
+    template = loader.get_template('post/post_detail.html')
+    # dict형 변수 context의 'post'키에 post(Post객체)를 할당
+    context = {
+        # context로 전달될 dict의 "키"값이 템플릿에서 사용가능한 변수명이 됨
+        'post': post,
+    }
+    # template에 인자로 주어진 context, request를 render함수를 사용해서 해당 template을 string으로 변환
+    rendered_string = template.render(context=context, request=request)
+    # 변환된 string을 HttpResponse형태로 돌려준다
+    return HttpResponse(rendered_string)
+
 
 
 def post_create(request):

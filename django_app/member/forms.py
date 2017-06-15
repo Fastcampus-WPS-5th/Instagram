4dd6052718d47d6a940ecad067e012c725045533
Lee <dev@azelf.com>
2017-06-15 17:24:25 +0900

    LoginForm을 사용

---
+++ django_app/member/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth import authenticate
+
+
+class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super().__init__(*args, **kwargs)
+
+    username = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': '사용자 아이디를 입력하세요',
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': '비밀번호를 입력하세요',
+            }
+        )
+    )
+
+    # is_valid를 실행했을 때, Form내부의 모든 field들에 대한
+    # 유효성 검증을 실행하는 메서드
+    def clean(self):
+        # clean()메서드를 실행한 기본결과 dict를 가져옴
+        cleaned_data = super().clean()
+        # username, password를 가져와 로컬변수에 할당
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        # username, password를 이용해 사용자 authenticate
+        user = authenticate(
+            username=username,
+            password=password
+        )
+        # 인증에 성공할 경우, Form의 cleaned_data의 'user'
+        # 키에 인증된 User객체를 할당
+        if user is not None:
+            self.cleaned_data['user'] = user
+        # 인증에 실패한 경우, is_valid()를 통과하지 못하도록
+        # ValidationError를 발생시킴
+        else:
+            raise forms.ValidationError(
+                'Login credentials not valid'
+            )
+        return self.cleaned_data
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth import \
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+from .forms import LoginForm
+
 User = get_user_model()
+
 
 def login(request):
     # member/login.html 생성
@@ -20,24 +23,36 @@ def login(request):
 
     # POST요청이 왔을 경우
     if request.method == 'POST':
+        ### Form클래스 미사용시
         # 요청받은 POST데이터에서 username, password키가 가진 값들을
         # username, password변수에 할당 (문자열)
-        username = request.POST['username']
-        password = request.POST['password']
+        # username = request.POST['username']
+        # password = request.POST['password']
+
         # authenticate함수를 사용해서 User객체를 얻어 user에 할당
         # 인증에 실패할 경우 user변수에는 None이 할당됨
-        user = authenticate(
-            request,
-            username=username,
-            password=password
-        )
+        # user = authenticate(
+        #     request,
+        #     username=username,
+        #     password=password
+        # )
         # user변수가 None이 아닐 경우 (정상적으로 인증되어 User객체를 얻은 경우)
-        if user is not None:
-            # Django의 session을 이용해 이번 request와 user객체를 사용해 로그인 처리
-            # 이후의 request/response에서는 사용자가 인증된 상태로 통신이 이루어진다
+        # if user is not None:
+        #     # Django의 session을 이용해 이번 request와 user객체를 사용해 로그인 처리
+        #     # 이후의 request/response에서는 사용자가 인증된 상태로 통신이 이루어진다
+        #     django_login(request, user)
+        #     # 로그인 완료후에는 post_list뷰로 리다이렉트 처리
+        #     return redirect('post:post_list')
+
+        ### Form클래스 사용시
+        #   Bound form생성
+        form = LoginForm(data=request.POST)
+        # Bound form의 유효성을 검증
+        if form.is_valid():
+            user = form.cleaned_data['user']
             django_login(request, user)
-            # 로그인 완료후에는 post_list뷰로 리다이렉트 처리
             return redirect('post:post_list')
+
         # user변수가 None일 경우 (username또는 password가 틀려 인증에 실패한 경우)
         else:
             # 로그인에 실패했음을 알림
@@ -49,7 +64,13 @@ def login(request):
         # 아닐경우 login.html을 render해서 리턴
         if request.user.is_authenticated:
             return redirect('post:post_list')
-        return render(request, 'member/login.html')
+        # LoginForm인스턴스를 생성해서 context에 넘김
+        form = LoginForm()
+        context = {
+            'form': form,
+        }
+        # render시 context에는 LoginForm클래스형 form객체가 포함됨
+        return render(request, 'member/login.html', context)
 
 
 def logout(request):

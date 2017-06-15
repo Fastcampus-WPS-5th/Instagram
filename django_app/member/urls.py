5afc28dc967c221dfa23f565d7e4a6138c40c226
Lee <dev@azelf.com>
2017-06-15 14:41:52 +0900

    login템플릿 관련 CSS수정

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'member'
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),

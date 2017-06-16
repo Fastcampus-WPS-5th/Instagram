dda0ba97d51db74d7ee8d71a0410b2f1f13cd173
Lee <dev@azelf.com>
2017-06-16 15:42:45 +0900

    User모델에 필드 추가

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     # 이 User모델을 AUTH_USER_MODEL로 사용하도록 settings.py에 설정
-    pass
+    nickname = models.CharField(max_length=24, null=True, unique=True)
+
+    def __str__(self):
+        return self.nickname

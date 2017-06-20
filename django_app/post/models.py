1c617a052818941878ccd44a7c6ed35a230c9033
Lee <dev@azelf.com>
2017-06-20 14:54:25 +0900

    Comment에 tags필드 추가

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -50,6 +50,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     content = models.TextField()
+    tags = models.ManyToManyField('Tag')
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     like_users = models.ManyToManyField(

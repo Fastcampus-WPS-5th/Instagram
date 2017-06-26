eb5b201a60c086764af69b7c3f7ad2a42b7526ff
Lee <dev@azelf.com>
2017-06-26 12:45:45 +0900

    get_user_info()로 GraphAPI에 요청하는 함수 구현

--- django_app/member/views/auth.py
+++ django_app/member/views/auth.py
@@ -140,6 +140,21 @@ def facebook_login(request):
         settings.FACEBOOK_SECRET_CODE,
     )
 
+    # Exception을 상속받아 CustomException을 생성
+    class GetAccessTokenException(Exception):
+        def __init__(self, *args, **kwargs):
+            error_dict = args[0]['data']['error']
+            self.code = error_dict['code']
+            self.message = error_dict['message']
+            self.is_valid = error_dict['is_valid']
+            self.scopes = error_dict['scopes']
+
+    class DebugTokenException(Exception):
+        def __init__(self, *args, **kwargs):
+            error_dict = args[0]['data']['error']
+            self.code = error_dict['code']
+            self.message = error_dict['message']
+
     def add_message_and_redirect_referer():
         """
         페이스북 로그인 오류 메시지를 request에 추가하고, 이전 페이지로 redirect
@@ -184,16 +199,55 @@ def facebook_login(request):
         # 액세스토큰 코드교환 결과에 오류가 있을 경우
         # 해당 오류를 request에 message로 넘기고 이전페이지 (HTTP_REFERER)로 redirect
         elif 'error' in result:
-            raise Exception(result['error'])
+            raise GetAccessTokenException(result)
         else:
             raise Exception('Unknown error')
+
+    def debug_token(token):
+        url_debug_token = 'https://graph.facebook.com/debug_token'
+        url_debug_token_params = {
+            'input_token': token,
+            'access_token': app_access_token
+        }
+        response = requests.get(url_debug_token, url_debug_token_params)
+        result = response.json()
+        if 'error' in result['data']:
+            raise DebugTokenException(result)
+        else:
+            return result
+
+    def get_user_info(user_id, token):
+        url_user_info = 'https://graph.facebook.com/v2.9/{user_id}'.format(user_id=user_id)
+        url_user_info_params = {
+            'access_token': token,
+        }
+        response = requests.get(url_user_info, params=url_user_info_params)
+        result = response.json()
+        return result
 
     # code키값이 존재하지 않으면 로그인을 더이상 진행하지 않음
     if not code:
         return add_message_and_redirect_referer()
     try:
+        # 이 view에 GET parameter로 전달된 code를 사용해서 access_token을 받아옴
+        # 성공시 access_token값을 가져옴
+        # 실패시 GetAccessTokenException이 발생
         access_token = get_access_token(code)
-    except Exception as e:
-        print(e)
+
+        # 위에서 받아온 access_token을 이용해 debug_token을 요청
+        # 성공시 토큰을 디버그한 결과 (user_id, scopes 등..)이 리턴
+        # 실패시 DebugTokenException이 발생
+        debug_result = debug_token(access_token)
+
+        # debug_result에 있는 user_id값을 이용해서 GraphAPI에 유저정보를 요청
+        user_info = get_user_info(user_id=debug_result['data']['user_id'], token=access_token)
+        print(user_info)
+
+    except GetAccessTokenException as e:
+        print(e.code)
+        print(e.message)
         return add_message_and_redirect_referer()
-        # 액세스토큰 검사
+    except DebugTokenException as e:
+        print(e.code)
+        print(e.message)
+        return add_message_and_redirect_referer()

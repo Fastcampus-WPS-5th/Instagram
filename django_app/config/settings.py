8d9abef80c305c43b7b137ca4154b7aca49546c9
Lee <dev@azelf.com>
2017-06-27 16:13:27 +0900

    PostgreSQL DB사용하도록 변경 및 pip 패키지추가

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -137,8 +137,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'instagram',
+        'PASSWORD': 'dlgksdud',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

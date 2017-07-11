c9455a021cc50a7a7c5b8e80840b028e11243baf
Lee <dev@azelf.com>
2017-07-11 12:38:09 +0900

    member app구조변경

---
+++ django_app/member/serializers/user.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from ..models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+# serializers의 __init__파일 구현
+# urls에 urls_apis, urls_views로 파일 구분
+# apis에 user.py모듈 생성, UserDetailView구현
+#   urls.urls_apis에 UserDetailView.as_view()를 연결
+
+# config.urls.urls_apis에 member.urls.urls_apis를 연결
